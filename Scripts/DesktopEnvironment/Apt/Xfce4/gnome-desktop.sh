12062f6527901edcf3c20b859fdcdcfacb4aab1d
Jeremy Wood <chuckwood0077@yahoo.com>
2020-12-15 01:00:30 -0600

    Update gnome-desktop.sh

--- Scripts/DesktopEnvironment/Apt/Xfce4/gnome-desktop.sh
+++ Scripts/DesktopEnvironment/Apt/Xfce4/gnome-desktop.sh
@@ -2,7 +2,7 @@
 
 #Get the necessary components
 apt-get update
-apt-get install gnome-ubuntu-desktop gnome-terminal tightvncserver -y
+apt-get install ubuntu-gnome-desktop gnome-terminal tightvncserver -y
 apt-get install xfe -y
 apt-get clean
 
